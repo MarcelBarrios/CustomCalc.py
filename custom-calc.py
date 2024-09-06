e5c620af3ea80bb9b7440b89e107c522be9ea2f0
Marcel Barrios <marcelbarrios1994@gmail.com>
2024-09-06 15:24:03 -0400

    file added

---
+++ custom-calc.py
@@ -0,0 +1,5 @@
+height = input("Please, introduce your heigh in inches: ")
+
+snowboardLength = float(height) * 2.54 * 0.88 
+
+print(f"The lenght of the snowboard in cm you should use based on your height is: {snowboardLength}")
